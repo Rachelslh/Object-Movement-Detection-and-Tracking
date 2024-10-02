5a461591e65925c898e87ae93d4b9b6b00348e6c
Racha Salhi <racha@agot.ai>
2024-10-02 17:42:26 +0100

    add gpu support in container

--- docker.sh
+++ docker.sh
@@ -9,4 +9,4 @@ docker build -t $IMAGE_NAME .
 
 
 # Run the Docker container
-docker run --rm --name $CONTAINER_NAME -it --ipc=host -v $(pwd):/app/ $IMAGE_NAME # Add --gpus all if you have a gpu, i personally have MPS which is not supported
+docker run --rm --name $CONTAINER_NAME -it --ipc=host --gpus all -v $(pwd):/app/ $IMAGE_NAME
